028c260bc75f9d47559f9ad3cd6e42c4a6b7320d
bebusl <bebus1998@naver.com>
2025-09-14 01:22:09 +0900

    feat: [FN-91]tanstack-router context에 zustand store 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-const router = createRouter({ routeTree });
+import useAuthStore, { type AuthState } from "@/stores/auth";
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
+    context: () => { auth: AuthState | undefined };
   }
 }
+const router = createRouter({
+  routeTree,
+  context: {
+    auth: undefined!,
+  },
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,11 +28,16 @@ function App() {
   return (
     <div className="min-w-dvw min-h-dvh p-8 bg-gray-50">
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <InnerApp />
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </div>
   );
+}
+
+function InnerApp() {
+  const auth = useAuthStore();
+  return <RouterProvider router={router} context={{ auth }} />;
 }
 
 export default App;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
+import type { AuthState } from "@/stores/auth";
 
-export const Route = createRootRoute({
+interface RouterContext {
+  auth: AuthState;
+}
+export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
 });
 
