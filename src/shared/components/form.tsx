57c38b1a37ffe529c98ce17d5f8405f3f440f407
bebusl <bebus1998@naver.com>
2025-09-18 13:30:37 +0900

    feat: [FN-172]form에서 보편적으로 쓰이는 컴포넌트 셋 추가

---
+++ src/shared/components/form.tsx
@@ -0,0 +1,41 @@
+import type { ReactNode } from "react";
+import { Label } from "./label";
+import * as React from "react";
+import { cn } from "@/shared/lib/utils";
+
+type FormTitleProps = {
+  children: ReactNode;
+};
+export const FormTitle = ({ children }: FormTitleProps) => {
+  return <h1 className="mb-4 mx-0 font-bold text-xl">{children}</h1>;
+};
+
+export const RequiredAsterisk = () => <span className="text-red-600">*</span>;
+
+type RequiredLabelProps = React.ComponentProps<typeof Label>;
+export const RequiredLabel = ({ ...props }: RequiredLabelProps) => {
+  return (
+    <div className={cn("flex items-center gap-1", props.className)}>
+      <Label {...props} />
+      <RequiredAsterisk />
+    </div>
+  );
+};
+
+type DescriptionProps = { children: ReactNode; className?: string };
+export const Description = ({ children, className }: DescriptionProps) => {
+  return (
+    <p className={cn("text-xs text-gray-500 mb-2 mx-auto", className)}>
+      {children}
+    </p>
+  );
+};
+
+type ErrorMessageProps = { children: ReactNode; className?: string };
+export const ErrorMessage = ({ children, className }: ErrorMessageProps) => {
+  return (
+    <p className={cn("text-xs text-red-500 mt-2 mx-auto", className)}>
+      {children}
+    </p>
+  );
+};
