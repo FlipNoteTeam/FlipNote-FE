ee8042553eb749c38e59e5f6327191eea5d27111
bebusl <bebus1998@naver.com>
2025-09-18 13:32:00 +0900

    feat: [FN-172]그룹 생성 폼 뷰로직 작성

--- src/pages/create-group.tsx
+++ src/pages/create-group.tsx
@@ -1,9 +1,24 @@
+import { GROUP_CATEGORY_MAP } from "@/domain/group";
 import type { GroupCategory } from "@/shared/apis";
 import { Button } from "@/shared/components/button";
+import {
+  ButtonCheckbox,
+  ButtonCheckboxGroupField,
+} from "@/shared/components/button-checkbox";
+import { Card, CardContent, CardHeader } from "@/shared/components/card";
 import { Checkbox } from "@/shared/components/checkbox";
+import {
+  Description,
+  ErrorMessage,
+  FormTitle,
+  RequiredLabel,
+} from "@/shared/components/form";
 import { Input } from "@/shared/components/input";
 import { Label } from "@/shared/components/label";
-import { useForm } from "react-hook-form";
+import { Textarea } from "@/shared/components/textarea";
+import BaseLayout from "@/shared/layouts/base-layout";
+
+import { useController, useForm } from "react-hook-form";
 
 type FormType = {
   name: string;
@@ -16,45 +31,120 @@ type FormType = {
 };
 
 const CreateGroup = () => {
-  const { register } = useForm<FormType>();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormType>();
+
+  const { field: categoryField } = useController({
+    name: "category",
+    control,
+    rules: {
+      required: "하나 이상의 카테고리를 선택해주세요",
+    },
+  });
+
+  const onSubmit = (data: FormType) => {
+    console.log(data);
+  };
 
   return (
-    <form>
-      <div>
-        <Label htmlFor="name">그룹명</Label>
-        <Input id="name" {...register("name")}></Input>
-      </div>
-      <div>
-        <Label htmlFor="category">카테고리</Label>
-        <Input id="category" {...register("category")}></Input>
-      </div>
-      <div>
-        <Label htmlFor="description">그룹 설명</Label>
-        <Input id="description" {...register("description")}></Input>
-      </div>
-      <div>
-        <Label htmlFor="applicationRequired">가입신청 여부</Label>
-        <Checkbox
-          className="h-8"
-          id="applicationRequired"
-          {...register("applicationRequired")}
-        ></Checkbox>
-      </div>
-      <div>
-        <Label htmlFor="publicVisible">공개</Label>
-        <Input id="publicVisible" {...register("publicVisible")}></Input>
-      </div>
-      <div>
-        <Label htmlFor="maxMember">최대 인원</Label>
-        <Input type="number" id="maxMember" {...register("maxMember")}></Input>
-      </div>
-      <div>
-        <Label htmlFor="image">이미지</Label>
-        <Input id="image" type="file" />
-      </div>
-
-      <Button type="submit">생성하기</Button>
-    </form>
+    <BaseLayout>
+      <Card className="py-8 px-4 gap-0">
+        <CardHeader>
+          <FormTitle>그룹 생성</FormTitle>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <RequiredLabel htmlFor="name">그룹명</RequiredLabel>
+              <Input
+                id="name"
+                {...register("name", { required: "그룹명을 입력해주세요" })}
+              />
+              {!!errors.name && (
+                <ErrorMessage>{errors.name.message}</ErrorMessage>
+              )}
+            </div>
+            <div>
+              <RequiredLabel>카테고리</RequiredLabel>
+              <Description>하나 이상의 카테고리를 선택해주세요</Description>
+              <ButtonCheckboxGroupField
+                name="category"
+                value={categoryField.value}
+                onChange={categoryField.onChange}
+                onBlur={categoryField.onBlur}
+              >
+                {Object.entries(GROUP_CATEGORY_MAP).map(([value, name]) => (
+                  <ButtonCheckbox key={value} value={value}>
+                    {name}
+                  </ButtonCheckbox>
+                ))}
+              </ButtonCheckboxGroupField>{" "}
+              {!!errors.category && (
+                <ErrorMessage>{errors.category.message}</ErrorMessage>
+              )}
+            </div>
+            <div>
+              <Label className="mb-2" htmlFor="description">
+                그룹 설명
+              </Label>
+              <Textarea id="description" {...register("description")} />
+            </div>
+            <div className="flex gap-2">
+              <Checkbox
+                id="applicationRequired"
+                {...register("applicationRequired")}
+              />
+              <Label htmlFor="applicationRequired">가입신청 여부</Label>
+            </div>
+            <div className="flex gap-2">
+              <Checkbox id="publicVisible" {...register("publicVisible")} />
+              <Label htmlFor="publicVisible">공개</Label>
+            </div>
+            <div>
+              <RequiredLabel htmlFor="maxMember" className="mb-2">
+                최대 인원
+              </RequiredLabel>
+              <div className="flex items-end">
+                <Input
+                  type="number"
+                  id="maxMember"
+                  className="w-20 inline"
+                  {...register("maxMember", {
+                    min: { value: 1, message: "최소 인원은 1명입니다." },
+                    max: { value: 100, message: "최대 인원은 100명입니다." },
+                  })}
+                />
+                <span className="text-sm ml-2 ">명</span>
+              </div>
+              {errors.maxMember && (
+                <ErrorMessage>{errors.maxMember.message}</ErrorMessage>
+              )}
+            </div>
+            <div>
+              <Label htmlFor="image" className="mb-2">
+                이미지
+              </Label>
+              <Input id="image" type="file" />
+            </div>
+            <div className="flex gap-2 justify-center">
+              <Button
+                type="button"
+                className="bg-white border border-gray-500 text-gray-500"
+              >
+                초기화
+              </Button>
+              <Button type="submit" className="bg-black text-white">
+                생성하기
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </BaseLayout>
   );
 };
 
