fa78be858b6323a8c32b97ead21e6e2a47307595
bebusl <bebus1998@naver.com>
2025-09-25 13:37:53 +0900

    feat: [FN-181] [FN-181] yjs 프로토콜 정의

---
+++ src/shared/socket/yjs-types.ts
@@ -0,0 +1,65 @@
+export interface YjsMessage {
+  type:
+    | "sync"
+    | "update"
+    | "awareness"
+    | "auth"
+    | "expired"
+    | "heartbeat"
+    | "access-control";
+  data?: unknown;
+}
+
+export interface SyncMessage extends YjsMessage {
+  type: "sync";
+  data: {
+    syncStep: number;
+    update?: Uint8Array;
+  };
+}
+
+export interface UpdateMessage extends YjsMessage {
+  type: "update";
+  data: {
+    update: Uint8Array;
+  };
+}
+
+export interface AwarenessMessage extends YjsMessage {
+  type: "awareness";
+  data: {
+    awareness: Uint8Array;
+  };
+}
+
+export interface AuthMessage extends YjsMessage {
+  type: "auth";
+  data: {
+    token: string;
+    userId: string;
+    documentId: string;
+  };
+}
+
+export interface ExpiredMessage extends YjsMessage {
+  type: "expired";
+  data: {
+    message: string;
+  };
+}
+
+export interface HeartbeatMessage extends YjsMessage {
+  type: "heartbeat";
+  data: {
+    timestamp: number;
+  };
+}
+
+export interface AccessControlMessage extends YjsMessage {
+  type: "access-control";
+  data: {
+    hasAccess: boolean;
+    currentEditor?: string;
+    message: string;
+  };
+}
