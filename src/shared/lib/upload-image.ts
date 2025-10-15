6409e17655199b61970f081f0ba6125eeea77fa2
bebusl <bebus1998@naver.com>
2025-10-16 05:01:41 +0900

    feat: [FN-132] 이미지 업로드 모듈 구현

--- src/shared/lib/upload-image.ts
+++ src/shared/lib/upload-image.ts
@@ -1,4 +1,30 @@
+import { imageApi, type IMAGE_UPLOAD_TYPE } from "@/shared/apis";
+import axios from "axios";
 import SparkMD5 from "spark-md5";
+
+type UploadImageProps = {
+  file: File;
+  type: IMAGE_UPLOAD_TYPE;
+};
+
+export const uploadImage = async ({ file, type }: UploadImageProps) => {
+  const fileName = await getFullHashFileName(file);
+  const res = await imageApi.getPresignedUrl({ fileName, type });
+  const { imageRefId, url } = res.data.data;
+
+  await axios.put(url, file, {
+    headers: { "Content-Type": file.type },
+  });
+
+  return imageRefId;
+};
+
+export const getFullHashFileName = async (file: File) => {
+  const fileHash = await getHashName(file);
+  const extension = getFileExtension(file);
+
+  return `${fileHash}.${extension}`;
+};
 
 export const getHashName = async (file: File): Promise<string> => {
   const arrayBuffer = await file.arrayBuffer();
