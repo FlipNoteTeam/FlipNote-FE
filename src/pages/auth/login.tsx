d58cf0e6878e0588456dc6d9beee3356791f3299
bebusl <bebus1998@naver.com>
2025-11-26 07:49:58 +0900

    design: [FN-234] 로그인 UI 개선

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,18 +1,14 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-} from "@/shared/components/card";
+import { Card, CardContent, CardFooter } from "@/shared/components/card";
 import { Input } from "@/shared/components/input";
 import { Button } from "@/shared/components/button";
 import { Label } from "@/shared/components/label";
-import { Separator } from "@/shared/components/separator";
 import { authApi, type UserLoginRequest } from "@/shared/apis";
 import { useNavigate, useSearch } from "@tanstack/react-router";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import useAuthStore from "@/stores/useAuthStore";
+import BaseLayout from "@/shared/layouts/base-layout";
+import { Sparkles } from "lucide-react";
 
 type FieldState = UserLoginRequest;
 
@@ -49,34 +45,69 @@ const Login = () => {
   };
 
   return (
-    <Card>
-      <CardHeader className="text-center">
-        <h3>FlipNote에 오신 것을 환영합니다!</h3>
-      </CardHeader>
-      <CardContent>
-        <Label htmlFor="email">이메일</Label>
-        <Input id="email" {...register("email")} />
-        <Label htmlFor="password">비밀번호</Label>
-        <Input type="password" id="password" {...register("password")}></Input>
-        {/* <Link to="/reset-password">비밀번호 찾기</Link> */}
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" variant="default" onClick={handleSubmit}>
-          로그인
-        </Button>
-        <Separator />
-        <p>또는 소셜 계정으로 로그인</p>
-        <Button variant="outline" className="w-full">
-          Login with Google
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Kakao
-        </Button>
-        <Button variant="outline" className="w-full">
-          Login with Github
-        </Button>
-      </CardFooter>
-    </Card>
+    <BaseLayout>
+      <div className="text-center space-y-3">
+        <div className="flex justify-center">
+          <div className="w-12 h-12 rounded-lg bg-primary flex items-center justify-center">
+            <Sparkles className="w-7 h-7 text-primary-foreground" />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold text-primary">FlipNote</h1>
+          <p className="text-sm text-muted-foreground mt-1">
+            FlipNote에 오신 걸 환영합니다!
+          </p>
+        </div>
+      </div>
+      <div className="text-center space-y-3">
+        <Card className="mt-4 py-16 px-8 max-w-md mx-auto">
+          <CardContent className="w-full space-y-4 ">
+            <div className="space-y-2">
+              <Label htmlFor="email">이메일</Label>
+              <Input id="email" {...register("email")} />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">비밀번호</Label>
+              <Input type="password" id="password" {...register("password")} />
+            </div>
+            {/* <Link to="/reset-password">비밀번호 찾기</Link> */}
+          </CardContent>
+          <CardFooter className="flex-col space-y-6 mt-4 ">
+            <Button
+              type="submit"
+              variant="default"
+              className="w-full"
+              onClick={handleSubmit}
+            >
+              로그인
+            </Button>
+
+            <div className="relative w-full">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-background text-muted-foreground">
+                  또는
+                </span>
+              </div>
+            </div>
+
+            <div className="w-full space-y-2">
+              <Button variant="outline" className="w-full">
+                Google로 로그인
+              </Button>
+              <Button variant="outline" className="w-full">
+                Kakao로 로그인
+              </Button>
+              <Button variant="outline" className="w-full">
+                Github로 로그인
+              </Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </BaseLayout>
   );
 };
 
