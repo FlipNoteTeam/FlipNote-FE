73fce073ede88e45e55403316cb40d4e3d614e86
bebusl <bebus1998@naver.com>
2025-11-26 07:36:45 +0900

    refactor: [FN-234] 로그인 라우트 네비게이션 시 타입안전성 강화

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -2,14 +2,19 @@ import Login from "@/pages/auth/login";
 import { authGuard } from "@/routes/__utils/authGuard";
 import { createFileRoute } from "@tanstack/react-router";
 
+type LoginSearch = {
+  redirect?: string;
+};
+
 export const Route = createFileRoute("/auth/login")({
   component: RouteComponent,
   beforeLoad: ({ context }) => {
     authGuard({ auth: context.auth, mode: "non-protected" });
   },
-  validateSearch: (search: Record<string, unknown>) => {
+  validateSearch: (search: Record<string, unknown>): LoginSearch => {
     return {
-      redirect: (search.redirect as string) || undefined,
+      redirect:
+        typeof search.redirect === "string" ? search.redirect : undefined,
     };
   },
 });
