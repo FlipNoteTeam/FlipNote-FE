d22fffad8c7785ac43f57fd3253f8a75f5e5940e
bebusl <bebus1998@naver.com>
2025-11-30 05:45:11 +0900

    design: [FN-230] gnb 스타일 업데이트

--- src/features/gnb/components/authenticated-nav.tsx
+++ src/features/gnb/components/authenticated-nav.tsx
@@ -18,18 +18,12 @@ const AuthenticatedNav = () => {
 
   if (!user) return null;
   return (
-    <ul className="flex items-center space-x-4">
-      <li className="text-md">
-        <Link to="/">대시보드</Link>
-      </li>
-      <li className="text-md">
-        <Link to="/">내 스터디</Link>
-      </li>
+    <ul className="flex items-center space-x-2">
       <li>
         <AlarmSheet>
           {({ unreadCount }) => (
             <Button variant="ghost" size="sm" className="relative">
-              <Bell className="h-4 w-4" />
+              <Bell className="h-4 w-4" strokeWidth={3} />
               {unreadCount > 0 && (
                 <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
                   {unreadCount > 99 ? "99+" : unreadCount}
@@ -41,7 +35,13 @@ const AuthenticatedNav = () => {
       </li>
       <li>
         <DropdownMenu>
-          <DropdownMenuTrigger>{user.name}</DropdownMenuTrigger>
+          <DropdownMenuTrigger className="font-semibold text-md">
+            <img
+              src={user.profileImageUrl}
+              className="w-4 h-4 object-fill rounded-full hover:bg-gray-100"
+            />
+            {/* {user.name} */}
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuGroup>
               <DropdownMenuItem asChild>
--- src/shared/layouts/gnb.tsx
+++ src/shared/layouts/gnb.tsx
@@ -1,4 +1,5 @@
 import AuthenticatedNav from "@/features/gnb/components/authenticated-nav";
+import NavItems from "@/features/gnb/components/nav-items";
 import UnauthenticatedNav from "@/features/gnb/components/unauthenticated-nav";
 import useAuthStore from "@/stores/useAuthStore";
 import { Link } from "@tanstack/react-router";
@@ -7,15 +8,18 @@ const GNB = () => {
   const user = useAuthStore((state) => state.user);
 
   return (
-    <header className="w-full bg-white shadow-sm border-b">
+    <header className="sticky top-0 z-50  backdrop-blur-sm">
       <nav className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
             <h1 className="text-xl font-semibold">
-              <Link to="/">FlipNote</Link>
+              <Link to="/">
+                <img src="/flipnote_logo_long.png" className="h-10" />
+              </Link>
             </h1>
           </div>
-          <ul className="flex items-center space-x-4">
+          <NavItems />
+          <ul className="flex items-center space-x-4 text-pri">
             {user ? <AuthenticatedNav /> : <UnauthenticatedNav />}
           </ul>
         </div>
