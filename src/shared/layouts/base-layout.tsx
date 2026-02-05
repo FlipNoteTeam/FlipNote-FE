94caea37e06ad30dfdba8daffd9d3062cd6d5900
bebusl <bebus1998@naver.com>
2026-02-05 12:55:20 +0900

    feat: [FN-294] base-layout 모바일 반응형 패딩 적용

--- src/shared/layouts/base-layout.tsx
+++ src/shared/layouts/base-layout.tsx
@@ -7,7 +7,7 @@ const BaseLayout = ({ children }: Props) => {
   return (
     <>
       <GNB />
-      <main className="max-w-7xl min-h-dvh p-16 m-auto">{children}</main>
+      <main className="max-w-7xl min-h-dvh px-4 py-6 sm:px-6 lg:px-16 lg:py-16 m-auto">{children}</main>
     </>
   );
 };
