41c8f96af7b129522b731f8ce8af39d1dc5cdcec
bebusl <bebus1998@naver.com>
2026-02-05 13:04:36 +0900

    style: [FN-294] unused import 제거

--- src/features/gnb/components/mobile-menu.tsx
+++ src/features/gnb/components/mobile-menu.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Menu } from "lucide-react";
 import { Button } from "@/shared/components/button";
 import {
   Sheet,
