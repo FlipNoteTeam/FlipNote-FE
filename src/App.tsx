f17a1cd93b4bc530799882cd705129cfb8051c8c
bebusl <bebus1998@naver.com>
2026-03-17 13:20:28 +0900

    feat: [FN-346] App에 Sonner Toaster 추가 (top-center, richColors)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "sonner";
 import { routeTree } from "./routeTree.gen";
 import useAuthStore from "@/stores/use-auth-store";
 import { type AuthState } from "@/stores/use-auth-store";
@@ -41,6 +42,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} context={{ auth }} />
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster position="top-center" richColors />
     </QueryClientProvider>
   );
 }
