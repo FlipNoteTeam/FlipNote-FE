cba2171af188b834f817d99f0b5a4704dce6f253
이진희 <49019236+bebusl@users.noreply.github.com>
2026-06-21 15:38:00 +0900

    refactor: gnb 네이밍 Component 컨벤션에 맞게 변경

--- src/shared/layouts/base-layout.tsx
+++ src/shared/layouts/base-layout.tsx
@@ -1,4 +1,4 @@
-import GNB from "@/shared/layouts/gnb";
+import GrobalNavigationBar from "@/shared/layouts/gnb";
 
 type Props = {
   children: React.ReactNode;
@@ -6,8 +6,10 @@ type Props = {
 const BaseLayout = ({ children }: Props) => {
   return (
     <>
-      <GNB />
-      <main className="max-w-7xl min-h-dvh px-4 py-6 sm:px-6 lg:px-16 lg:py-16 m-auto">{children}</main>
+      <GrobalNavigationBar />
+      <main className="max-w-7xl min-h-dvh px-4 py-6 sm:px-6 lg:px-16 lg:py-16 m-auto">
+        {children}
+      </main>
     </>
   );
 };
