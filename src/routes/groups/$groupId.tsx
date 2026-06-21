2b7afabcca282680595d2ef363fb1ee6cb4f9095
이진희 <49019236+bebusl@users.noreply.github.com>
2026-06-21 14:57:43 +0900

    feat: 그룹 레이아웃 라우트 도입 및 카드셋 슬라이드업 모달 통합
    $groupId.tsx 레이아웃 라우트 추가: GroupDetailPage를 배경으로 유지하면서 Outlet에
    CardsetDetailSheet를 렌더. /manage 경로에서는 레이아웃 bypass해 ManagePage만 표시.
    $groupId/index.tsx는 null 반환 (그룹 콘텐츠는 레이아웃에서 담당).
    $cardsetId/index.tsx는 CardsetDetailSheet만 렌더하도록 변경.

---
+++ src/routes/groups/$groupId.tsx
@@ -0,0 +1,23 @@
+import { createFileRoute, Outlet, useRouterState } from "@tanstack/react-router";
+import GroupDetail from "@/pages/group-detail";
+
+export const Route = createFileRoute("/groups/$groupId")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { groupId } = Route.useParams();
+  const pathname = useRouterState({ select: (s) => s.location.pathname });
+  const isManage = pathname.endsWith("/manage");
+
+  if (isManage) {
+    return <Outlet />;
+  }
+
+  return (
+    <>
+      <GroupDetail id={groupId} />
+      <Outlet />
+    </>
+  );
+}
